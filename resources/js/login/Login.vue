37743426cb1469454a321dfffe2d43023b4ada48
JRS050 <rampinojonathan@gmail.com>
2025-09-03 15:47:41 +0200

    Setup

---
+++ resources/js/login/Login.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { ref } from 'vue';
+
+
+const user = ref({
+    name:'',
+    password:''
+}) 
+
+const submit = () =>{};
+</script>
+<template>
+    <form>
+        <input v-model="user.name" required type="text"></input>
+        <input v-model="user.password" required type="password"></input>
+        <button @click="submit"></button>
+    </form>
+</template>
