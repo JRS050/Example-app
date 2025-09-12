980dc376e375af49790891a50b3c95da9a579494
JRS050 <rampinojonathan@gmail.com>
2025-09-12 13:42:15 +0200

    Fetching user data

--- resources/js/login/pages/User.vue
+++ resources/js/login/pages/User.vue
@@ -1,21 +1,24 @@
 <script setup lang="ts">
 import axios from 'axios';
-import { computed } from 'vue';
+import {  ref } from 'vue';
+
+const user = ref();
+const error = ref();
 
 const fetchUserData = async () => {
   try {
     const response = await axios.get('/api/me');
-    console.log('User Data:', response.data);
-    return response;
+    user.value = response.data;
    
   } catch (error) {
     console.error('Error fetching user data:', error);
     return error;
   }
 };
-const user = computed(()=>fetchUserData());
+
 
 </script>
 <template>
-    <p>{{ user }}</p>
+  <button @click="fetchUserData">Fetch logged user data</button>
+      <p v-if="user">{{ user }}</p>
 </template>
